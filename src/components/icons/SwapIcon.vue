96cd35d230a9adbb4f79122e0a4c409949e601df
“mcurren” <mike@curren.me>
2023-02-23 14:53:00 -0600

    adding icons for later use

---
+++ src/components/icons/SwapIcon.vue
@@ -0,0 +1,13 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="20"
+    viewBox="0 96 960 960"
+    width="20"
+  >
+    <path
+      d="M288 888 96 696l192-192 51 51-105 105h294v72H234l105 105-51 51Zm384-240-51-51 105-105H432v-72h294L621 315l51-51 192 192-192 192Z"
+      fill="currentColor"
+    />
+  </svg>
+</template>
